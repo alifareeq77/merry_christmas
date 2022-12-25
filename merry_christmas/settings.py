fd40b994500d82fb7084dde25bddeb4b80dda550
alifareeq77 <alifareeq77@gmail.com>
2022-12-26 01:51:39 +0300

    deploy settings

--- merry_christmas/settings.py
+++ merry_christmas/settings.py
@@ -1,7 +1,8 @@
 import os
 from pathlib import Path
 import mimetypes
-
+import django_heroku
+import dj_database_url
 mimetypes.add_type("text/css", ".css", True)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -13,9 +14,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t79l4s6l3wm_sm1zan=o*(fzhrc41!ra+kqmr0&9f%=uvxw8n0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -98,6 +99,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
-
+django_heroku.settings(locals())
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
