aa6c6463a5e8cb852f3f8d49ddbcf7afc6548a63
alifareeq77 <alifareeq77@gmail.com>
2022-12-26 02:03:14 +0300

    d

--- merry_christmas/settings.py
+++ merry_christmas/settings.py
@@ -1,9 +1,8 @@
 import os
 from pathlib import Path
-import mimetypes
 import django_heroku
 import dj_database_url
-mimetypes.add_type("text/css", ".css", True)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -99,7 +98,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 django_heroku.settings(locals())
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
